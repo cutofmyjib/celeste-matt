072733d4cfc5d4587d7accf87c991d4e03af4b20
cutofmyjib <thedianalozano@gmail.com>
2016-01-16 00:55:16 -0800

    add accomodations route

--- react-starter-master/app/App.js
+++ react-starter-master/app/App.js
@@ -5,6 +5,7 @@ import Ceremony from './ceremony.js';
 import Home from './home.js';
 import Wrapper from './wrapper.js'
 import Registry from './registry.js'
+import Accomodations from './accomodations.js'
 import Photos from './photos.js'
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
         <Route component={Wrapper}>
           <Route path="/" component={Home} />
           <Route path="/details" component={Ceremony} />
+          <Route path="/accomodations" component={Accomodations} />
           <Route path="/registry" component={Registry} />
           <Route path="/photos" component={Photos} />
         </Route>
