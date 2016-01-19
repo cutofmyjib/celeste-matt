1745a8715c54251a94d59b9a9cc59dfc69c96341
cutofmyjib <thedianalozano@gmail.com>
2016-01-18 23:48:04 -0800

    our story initial page

---
+++ react-starter-master/app/ourstory.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+
+export default class Ourstory extends Component {
+  render() {
+    return (
+      <div className='main'>
+        <h1 className='main-title'>OUR STORY</h1>
+      </div>
+    );
+  }
+}
