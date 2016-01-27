f3f06eee296e78af9e7936f0ae2f11533936b528
cutofmyjib <thedianalozano@gmail.com>
2016-01-26 16:51:00 -0800

    change span to ul and li tags

--- react-starter-master/app/header.js
+++ react-starter-master/app/header.js
@@ -3,37 +3,39 @@ import { Router, Route, Link } from 'react-router'
 import {render} from 'react-dom';
 
 export default class Header extends Component {
-  handleClick() {
-    this.setState({open: !this.state.open});
-  }
-
   constructor() {
     super();
     this.handleClick = this.handleClick.bind(this);
     this.state = { open: false }
   }
 
+  handleClick() {
+    this.setState({open: !this.state.open});
+  }
+
+
   render() {
     return (
       <header>
         <div className='header-rose'></div>
         <nav onClick={this.handleClick} className='nav-menu'>
-          <span className={"nav-menu-container " + (this.state.open ? "show" : "") } >
-            <span className="nesting-nav-wrapper"><Link to="/">Our Wedding</Link>
-              <span className="sub-nav">
-                <Link to="/ourstory">Our Story</Link>
-              </span>
-            </span>
-            <Link to="/rsvp">RSVP</Link>
-            <span className="nesting-nav-wrapper"><Link to="/details">Details</Link>
-              <span className="sub-nav">
-                <Link to="/accomodations">Accomodations</Link>
-              </span>
-            </span>
-            <Link to="/registry">Registry</Link>
-            <Link to="/photos">Photos</Link>
-
-          </span>
+          <ul className={"nav-menu-container " + (this.state.open ? "show" : "") }>
+            <li className="nesting-nav-wrapper">Our Wedding
+              <ul className="sub-nav">
+                <li><Link to="/">Save the Date</Link></li>
+                <li><Link to="/ourstory">Our Story</Link></li>
+              </ul>
+            </li>
+            <li><Link to="/rsvp">RSVP</Link></li>
+            <li className="nesting-nav-wrapper">Details
+              <ul className="sub-nav">
+                <li><Link to="/details">Details</Link></li>
+                <li><Link to="/accomodations">Accomodations</Link></li>
+              </ul>
+            </li>
+            <li><Link to="/registry">Registry</Link></li>
+            <li><Link to="/photos">Photos</Link></li>
+          </ul>
         </nav>
       </header>
       )
