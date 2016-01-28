5ec339c9ad29e65aecd1d78e333d5536e517c676
cutofmyjib <thedianalozano@gmail.com>
2016-01-28 02:27:36 -0800

    add ourstory component

--- react-starter-master/app/ourstory.js
+++ react-starter-master/app/ourstory.js
@@ -6,6 +6,7 @@ export default class Ourstory extends Component {
     return (
       <div className='main'>
         <h1 className='main-title'>OUR STORY</h1>
+        <p>text</p>
       </div>
     );
   }
