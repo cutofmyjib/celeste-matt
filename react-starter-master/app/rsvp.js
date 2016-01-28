aab6cdd646e5336fb323d5d855dfbd92256db335
cutofmyjib <thedianalozano@gmail.com>
2016-01-28 02:28:59 -0800

    rsvp component

---
+++ react-starter-master/app/rsvp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+
+export default class Ceremony extends Component {
+  render() {
+    return (
+      <div className='main'>
+        RSVP HERE
+      </div>
+    );
+  }
+}
