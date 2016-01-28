51417141ca86578ea79e0db02c2b6b706e13086d
cutofmyjib <thedianalozano@gmail.com>
2016-01-28 02:26:09 -0800

    add routes

--- react-starter-master/app/App.js
+++ react-starter-master/app/App.js
@@ -3,8 +3,10 @@ import { browserHistory, Router, Route, Link } from 'react-router'
 import {render} from 'react-dom';
 import Ceremony from './ceremony.js';
 import Home from './home.js';
+import Ourstory from './ourstory.js';
 import Wrapper from './wrapper.js'
 import Registry from './registry.js'
+import Rsvp from './rsvp.js'
 import Accomodations from './accomodations.js'
 import Photos from './photos.js'
 
@@ -14,7 +16,9 @@ class App extends Component {
       <Router history={browserHistory}>
         <Route component={Wrapper}>
           <Route path="/" component={Home} />
+          <Route path="/ourstory" component={Ourstory} />
           <Route path="/details" component={Ceremony} />
+          <Route path="/rsvp" component={Rsvp} />
           <Route path="/accomodations" component={Accomodations} />
           <Route path="/registry" component={Registry} />
           <Route path="/photos" component={Photos} />
