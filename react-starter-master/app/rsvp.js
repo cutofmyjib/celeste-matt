03c5f4aec7607a64c119ce2ce34d0c6e45eb8dc8
cutofmyjib <thedianalozano@gmail.com>
2016-02-20 17:24:37 -0800

    embed rsvpify iframe

--- react-starter-master/app/rsvp.js
+++ react-starter-master/app/rsvp.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import {render} from 'react-dom';
 
 export default class Ceremony extends Component {
+
   render() {
     return (
       <div className='main'>
-        RSVP HERE
+        <h1 className='main-title'>RSVP</h1>
+        <small>Code: loveatfirstonsight</small>
+        <div className='rsvp'>
+          <iframe src="http://CelesteandMatt.rsvpify.com/" scrolling="yes" frameBorder="0"></iframe>
+        </div>
       </div>
     );
   }
