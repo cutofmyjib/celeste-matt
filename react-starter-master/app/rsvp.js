6a36f0b5c2399c1f4a8e51212381ab5c264f5df2
cutofmyjib <thedianalozano@gmail.com>
2016-02-20 18:34:29 -0800

    for firebase

--- react-starter-master/app/rsvp.js
+++ react-starter-master/app/rsvp.js
@@ -9,7 +9,7 @@ export default class Ceremony extends Component {
         <h1 className='main-title'>RSVP</h1>
         <small>Please enter code: loveatfirstonsight</small>
         <div className='rsvp'>
-          <iframe src="http://CelesteandMatt.rsvpify.com/" scrolling="yes"></iframe>
+          <iframe src="https://CelesteandMatt.rsvpify.com/" scrolling="yes"></iframe>
         </div>
       </div>
     );
