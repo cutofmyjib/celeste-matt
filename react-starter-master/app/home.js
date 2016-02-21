7f39b05c2e25e6504b6ad98967bf8fbb4139b922
cutofmyjib <thedianalozano@gmail.com>
2016-02-20 17:24:01 -0800

    cleanup

--- react-starter-master/app/home.js
+++ react-starter-master/app/home.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { Router, Route, Link } from 'react-router'
-import {render} from 'react-dom';
 
 export default class Home extends Component {
   render() {
