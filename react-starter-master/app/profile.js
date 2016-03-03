55e9170afea36b64c789261ba64f676189898bdf
cutofmyjib <thedianalozano@gmail.com>
2016-03-02 23:18:28 -0800

    add div for profile img

--- react-starter-master/app/profile.js
+++ react-starter-master/app/profile.js
@@ -4,9 +4,12 @@ export default class Profile extends Component {
   render() {
     return (
       <div className='story profile'>
-        <h2>{this.props.name}</h2>
-        <h2><i>{this.props.title}</i></h2>
-        <p>{this.props.desc}</p>
+        <div className='profile-pic'><img src='http://thecatapi.com/api/images/get?format=src&type=jpg' /></div>
+        <div className='profile-content'>
+          <h2>{this.props.name}</h2>
+          <h2><i>{this.props.title}</i></h2>
+          <p>{this.props.desc}</p>
+        </div>
       </div>
     );
   }
