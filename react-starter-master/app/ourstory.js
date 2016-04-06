ebeeefae4d4c8c18d0eea310ff59506983de0cad
cutofmyjib <thedianalozano@gmail.com>
2016-04-06 16:27:03 -0700

    add photo

--- react-starter-master/app/ourstory.js
+++ react-starter-master/app/ourstory.js
@@ -30,7 +30,10 @@ export default class Ourstory extends Component {
     return (
       <div className='main'>
         <h1 className='main-title'>OUR STORY</h1>
-            {stories}
+        <div className='story'>
+          <img className='img-large' src='https://lh3.googleusercontent.com/cmzZD4gHd5hBIGl5qPt5zhTgR6qnikhANa7JxTLcptZYWuj8y_nFxeekK403g-vEIJgkLQKWrsSsMmicjp5t-nGGbrd7xqWMHdXURMyd_pl5eEmcDrwzhsP3cx56FSpNOFGiv8lr_hkCsiQOYNtT9e18SoTDArdT4yoMOD3lVz4ytZ3fmLN6L9iF3Z5qGySiImX5E64eqeIr2C7TgnjL2AKqDvrPsZNZqjYKuE7icg-chq2uBlOraTfVz58UoPtxSgCXsjBV9aLEhCWHKbyBpi4WpgLoBd1HKuGYPE5kqqa4FBFB76N9s6BtYW4lubi2eS08FwEqp_k6zGiCyWmzTC81WW26zfmQQfq2hZE-B4CQ-8gCsI9KwBwk7Ne-Oqrd4OJh9JoOlog8lwSbCVdLjHm1vn8KFfx6ob-Eym6JXiecsqesfbnLOaly2H0Fw5ZEqXY4vbOqX77P01HVgpTSvtfw2scyS5em-TDUG3wp4kHfgjglKwd9KnjMu_IZUadicAvaVeIlh9xWKG7OSMSuW_YVgKv2HtS2aAV6uQaJ4QmoUaAtySKT3u5-8JtOzA5mJWsR=w1966-h980-no' />
+        </div>
+        {stories}
         <div className='break-heartknot'></div>
       </div>
     );
