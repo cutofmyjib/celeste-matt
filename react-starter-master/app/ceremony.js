03c8a9db92cf0b5e4d4756992174c895bb2ca863
cutofmyjib <thedianalozano@gmail.com>
2016-04-06 16:23:04 -0700

    google map link

--- react-starter-master/app/ceremony.js
+++ react-starter-master/app/ceremony.js
@@ -16,13 +16,13 @@ export default class Ceremony extends Component {
             <div className='break-fig8'></div>
             <h2>Ceremony</h2>
             <p>Wisconsin Conservatory of Music</p>
-            <p>1584 N Prospect Ave, Milwaukee, WI 53202</p>
+            <p><a href="http://maps.google.com/?q=1584 N Prospect Ave, Milwaukee, WI 53202">1584 N Prospect Ave, Milwaukee, WI 53202</a></p>
             <p className='p-note'>Parking available to first 20 cars <br/> <a href="http://maps.google.com/?q=1684 N Prospect Ave, Milwaukee, WI 53202">1684 N Prospect Ave, Milwaukee, WI 53202</a></p>
             <div className='break-fig8'></div>
             <h2>Cocktail/Reception</h2>
             <p>5:30 - 6 pm</p>
             <p>Best Place at the Historic Pabst Brewery</p>
-            <p>901 W Juneau Ave, Milwaukee, WI 53233</p>
+            <p><a href="http://maps.google.com/?q=901 W Juneau Ave, Milwaukee, WI 53233">901 W Juneau Ave, Milwaukee, WI 53233</a></p>
             <p className='p-note'>Street and garage parking available <br/> <a href="http://maps.google.com/?q=1213 N 9th St, Milwaukee, WI 53233">1213 N 9th St, Milwaukee, WI 53233</a></p>
             <h2 className='h2-italic'>Dinner and Dancing to follow</h2>
             <p>6:30 pm</p>
