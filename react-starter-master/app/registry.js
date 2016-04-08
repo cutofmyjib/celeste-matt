09f93dc6b7ddc66b8d879a4fce3eaac125a53733
cutofmyjib <thedianalozano@gmail.com>
2016-04-07 18:54:55 -0700

    add div

--- react-starter-master/app/registry.js
+++ react-starter-master/app/registry.js
@@ -5,12 +5,14 @@ export default class Home extends Component {
     return (
       <div className='main'>
         <h1 className='main-title'>REGISTRY</h1>
-        <p className='p-blurb'>
-          To our family and friends, we are so happy to announce our wedding and would love for you to attend!
-          Picking a wedding gift can be a challenge, we have registered items we would love to have in our home
-          to make it hassle-free. Just click on any of the store logos below and it’ll take you to our
-          registry page.
-        </p>
+        <div className='story details'>
+          <p className='p-note'>
+            To our family and friends, we are so happy to announce our wedding and would love for you to attend!
+            Picking a wedding gift can be a challenge, we have registered items we would love to have in our home
+            to make it hassle-free. Just click on any of the store logos below and it’ll take you to our
+            registry page.
+          </p>
+        </div>
         <ul className='registry'>
           <li><a href="https://www.amazon.com/registry/wedding/27E2LNVWRO8W1" target="_blank"><img src='../style/rgstry-amazon.png' /></a></li>
           <li><a href="http://www.rei.com/GiftRegistryDetails/GR128358809" target="_blank"><img src='../style/rgstry-rei.png' /></a></li>
