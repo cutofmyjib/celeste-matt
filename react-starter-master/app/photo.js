b26bb7669b021c63c92000dbdca2dc3d2037496f
cutofmyjib <thedianalozano@gmail.com>
2016-04-10 16:51:13 -0700

    use react-modal

--- react-starter-master/app/photo.js
+++ react-starter-master/app/photo.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
 
 export default class Photo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalIsOpen: false
+    };
+  }
+
+  openModal() {
+    this.setState({ modalIsOpen: true })
+  }
+
+  closeModal() {
+    this.setState({ modalIsOpen: false })
+  }
+
   render() {
     return (
       <div>
-        <li><img src={this.props.url} /></li>
+        <div>
+          <li onClick={ this.openModal.bind(this) } ><img src={this.props.url} /></li>
+        </div>
+        <Modal isOpen={ this.state.modalIsOpen } onRequestClose={ this.closeModal.bind(this) }>
+          <div><img className='img-large' src={this.props.url} /></div>
+        </Modal>
       </div>
     );
   }
